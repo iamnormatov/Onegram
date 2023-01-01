d7541206ad77f7e856124d18bb8a99671c65afa0
New-Bucket <azizjonnormatov437@gmail.com>
2023-01-01 12:49:02 +0500

    file

--- src/components/profile/ModalComments.js
+++ src/components/profile/ModalComments.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { getUserByUsername } from './../../services/firebase';
-
+ 
 const ModalComments = ({ comment, redirect, setComments }) => {
     const [ user, setUser ] = useState({});
 
