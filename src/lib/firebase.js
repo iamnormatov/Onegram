0dfb3d188f88ce227c96e8aa69cd81666aaca809
New-Bucket <azizjonnormatov437@gmail.com>
2023-01-01 12:57:43 +0500

    file

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -3,7 +3,7 @@ import firebaseAuthServices from "firebase/auth";
 import "firebase/compat/firestore";
 import "firebase/compat/auth"
 import "firebase/compat/storage";
-
+ 
 const config = {
     apiKey: "AIzaSyD9bey1ez6gx_2he0MpCL3Dw2GKoaimDt8",
     authDomain: "instagram-dc39d.firebaseapp.com",
