99bfff70d934a135b241df268ee7ce4358397551
New-Bucket <azizjonnormatov437@gmail.com>
2023-01-01 12:55:49 +0500

    file

--- src/icons/SignOut.js
+++ src/icons/SignOut.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+ 
 const SignOut = () => {
     return (
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" className="w-6 h-6">
