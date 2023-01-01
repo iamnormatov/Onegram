8183c676cde2e34756a4dcf5e1e4694361e3cb15
New-Bucket <azizjonnormatov437@gmail.com>
2023-01-01 12:54:39 +0500

    file

--- src/helpers/ProtectedRoute.js
+++ src/helpers/ProtectedRoute.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Navigate } from 'react-router-dom';
 import { LOGIN } from '../constants/routes';
-
+ 
 const ProtectedRoute = ({ user, children}) => {
     if (!user) {
         return <Navigate to={LOGIN} replace />;
