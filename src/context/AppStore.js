4999534df31bf5249f2f84326c00c95dfe8b964e
New-Bucket <azizjonnormatov437@gmail.com>
2023-01-01 12:53:29 +0500

    file

--- src/context/AppStore.js
+++ src/context/AppStore.js
@@ -3,7 +3,7 @@ import React, { useState, useContext, createContext } from 'react';
 const userState = {};
 
 export const Context = createContext();
-
+ 
 const AppStore = ({ children }) => {
     const [user, setUser] = useState(userState);
 
