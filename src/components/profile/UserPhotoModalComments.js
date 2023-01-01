29517520cf748a1b32311411bd9cc146de3fdf8e
New-Bucket <azizjonnormatov437@gmail.com>
2023-01-01 13:00:39 +0500

    All the

--- src/components/profile/UserPhotoModalComments.js
+++ src/components/profile/UserPhotoModalComments.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from "react-router-dom";
 import ModalComments from './ModalComments';
-
+ 
 const UserPhotoModalComments = ({ photo, setShowModal }) => {
     const [ comments, setComments ] = useState(photo.comments);
     const navigate = useNavigate();
